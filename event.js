6e308ddd32e444aa338650ce722b3c6fcae2fa22
unknown <pkspyder007@gmail.com>
2022-11-02 23:25:36 +0530

    adds support for deduping images using api

--- event.js
+++ event.js
@@ -1,27 +1,26 @@
-// caption start
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.todo == "activate") {
-        chrome.tabs.query({
-            active: true,
-            currentWindow: true
-        }, function (tabs) {
-            chrome.pageAction.show(tabs[0].id)
-        })
-    }
+  if (message.todo == "activate") {
+    chrome.tabs.query({
+      active: true,
+      currentWindow: true
+    }, function (tabs) {
+      chrome.pageAction.show(tabs[0].id)
+    })
+  }
 })
 
-chrome.storage.onChanged.addListener(function(result, namespaces){
-    if (result.subtitleWarning) {
-        if(result.subtitleWarning.newValue){
-            var notifOption = {
-                type: "basic",
-                iconUrl: "round-table.png",
-                title: "Caption Off!",
-                message: "Please turn on CAPTIONS!!"
-            };
-            chrome.notifications.create("captionOff", notifOption);
-        }  
+chrome.storage.onChanged.addListener(function (result, namespaces) {
+  if (result.subtitleWarning) {
+    if (result.subtitleWarning.newValue) {
+      var notifOption = {
+        type: "basic",
+        iconUrl: "round-table.png",
+        title: "Caption Off!",
+        message: "Please turn on CAPTIONS!!"
+      };
+      chrome.notifications.create("captionOff", notifOption);
     }
+  }
 })
 
 // caption end
@@ -44,12 +43,52 @@ function messageResponder(req, senderInfo, cb) {
   }
 }
 
+// compare images
+async function compareImages(img1, img2) {
+  try {
+    deepai.setApiKey('<add_your_api_key_here>');
+
+    let node1 = document.createElement('img');
+    node1.src = img1;
+    let node2 = document.createElement('img');
+    node2.src = img2;
+    let resp = await deepai.callStandardApi("image-similarity", {
+      image1: node1,
+      image2: node2,
+    });
+
+    if(resp?.output?.distance > 30) {
+      return true;
+    }
+    
+    return false;
+
+  } catch (error) {
+    console.log(error);
+    return true
+  }
+}
 
 function takeScreenshot() {
-    chrome.tabs.captureVisibleTab(null, { format: "jpeg"}, (screenshotUrl) => {
-        if(screenshotUrl) {
-            screenshots.push(screenshotUrl);
+  chrome.tabs.captureVisibleTab(null, { format: "jpeg" }, async (screenshotUrl) => {
+    if (!screenshotUrl) {
+      return;
     }
+    if (screenshots.length == 0) {
+      screenshots.push(screenshotUrl);
+      return;
+    }
+
+    // screenshots.push(screenshotUrl);
+
+    const res = await compareImages(screenshots[screenshots.length - 1], screenshotUrl);
+    if(res) {
+      screenshots.push(screenshotUrl);
+      console.log("addding screenshot");
+    }
+    console.log("skipping screenshot");
+
+
   });
 }
 
@@ -57,9 +96,9 @@ function openScreenShotsInNewTab() {
   const viewTabUrl = chrome.extension.getURL('popup.html');
   const views = chrome.extension.getViews();
   for (let i = 0; i < views.length; i++) {
-      let view = views[i];
+    let view = views[i];
     if (view.document.URL == viewTabUrl) {
-        view.setScreenshotUrl(screenshots);
+      view.setScreenshotUrl(screenshots);
       break;
     }
   }
